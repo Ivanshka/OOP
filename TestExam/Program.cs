4bf5c259d9deef7be396da2a49c1a1bdbc198e22
Иван Павлович <pavlovich.ivan.2000@mail.ru>
2020-01-12 01:59:45 +0300

    Забабахал 2 билета с экзамена в отдельных проектах.

---
+++ TestExam/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+/*
+1. Создайте интерфейс Figure с виртуальным методом print. Создать класс Rectangle с
+координатами x, y, длиной и шириной h, l и color (строка) цветом и реализацией
+интерфейса. Метод print должен выводить прямоугольник на консоль.
+2. Класс должен содержать: конструктор без параметров и с тремя, пятью
+параметрами (используйте делигирование конструкторов), переопределите метода
+ToString, оператор + int (добавление к ширине и высоте целого числа), метод
+вычисления площади.
+3. Создайте коллекцию List<> из 6 прямоугольников. Продемонстрируйте работу
+оператора + и метода print.
+4. Используя LINQ отсортируйте ее по x, а затем по y, затем по площади, возьмите
+первый и последний объекты и выведите их.
+5. Сериализуйте коллекцию в формате Json, координаты x, y — сделайте не сериализуемыми.
+*/
+namespace TestExam
+{
+    interface Figure
+    {
+        void print();
+    }
+
+    [DataContract]
+    class Rectangle : Figure
+    {
+        //int x, y, w, h;
+
+        [DataMember]
+        public string Color { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        [DataMember]
+        public int W { get; set; }
+        [DataMember]
+        public int H { get; set; }
+
+        public void print()
+        {
+            Console.WriteLine("Я тип прямоугольник на консоли)");
+        }
+
+        public Rectangle() { X = Y = W = H = 0; Color = "Black"; }
+        public Rectangle(int x, int y, string color) { X = x; Y = y; Color = color; }
+        public Rectangle(int x, int y, string color, int w, int h):this(x, y, color) { W = w; H = h; }
+
+        public override string ToString() { return $"X: {X}, Y: {Y}, W: {W}, H: {H}, Color: {Color}"; }
+
+        public static Rectangle operator +(Rectangle r, int a) { r.H += a; r.W += a; return r; }
+
+        public int GetArea() { return W * H; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            List<Rectangle> list = new List<Rectangle>();
+            Console.WriteLine("Список");
+            for (int i = 0; i < 6; i++)
+            {
+                list.Add(new Rectangle(r.Next(), r.Next(), "Black", r.Next(), r.Next()));
+                Console.WriteLine(list[i]);
+            }
+            var sort = from l in list
+                       orderby l.X, l.Y, l.GetArea()
+                       select l;
+
+            Console.WriteLine("Первый и последний");
+            Console.WriteLine(sort.First());
+            Console.WriteLine(sort.Last());
+
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(List<Rectangle>));
+            using (FileStream fs = new FileStream("data.json", FileMode.OpenOrCreate))
+            {
+                serializer.WriteObject(fs, list);
+            }
+
+            System.Diagnostics.Process.Start("data.json");
+            Console.WriteLine("Сериализовано!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestExam2/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+/*
+№ 2-6
+1. Создать класс User с закрытыми полями email и password, status (может содержать значения signin или signout).
+2. Переопределить в классе все стандартного public методы Object. Перегрузить метод CompareTo стандартного
+унаследованного интерфейса IComparable, который сравнивает пользователей по email. Создать пять пользователей и
+сравнить их.
+3. Создать класс WebNet, который содержит LinkedList<> всех пользователей с методами добавления и удаления.
+Создайте объект github и добавьте в список всех пользователей.
+4. Используя LINQ посчитайте сколько пользователей зашли на ресурс.
+5. Сериализуйте этих пользователей в любом формате.
+*/
+
+namespace TestExam2
+{
+    enum State { signin, signout }
+
+    [DataContract]
+    class User: IComparable
+    {
+        [DataMember]
+        string email;
+        [DataMember]
+        string password;
+        public State status;
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is User))
+                return -1;
+            User temp = (User)obj;
+            return email.CompareTo(temp.email);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is User))
+                return false;
+            User temp = (User)obj;
+            if (email == temp.email)
+                if (password == temp.password)
+                    return true;
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return email.Length*password.Length;
+        }
+        public override string ToString()
+        {
+            return $"Email: {email}, status: {status}";
+        }
+
+        public User(string email, string password, State state)
+        {
+            this.email = email; this.password = password; status = state;
+        }
+    }
+
+    [DataContract]
+    class WebNet
+    {
+        [DataMember]
+        public LinkedList<User> users;
+        
+        public WebNet()
+        {
+            users = new LinkedList<User>();
+        }
+
+        public void Add (User user) => users.AddFirst(user);
+        public void Remove (User user) => users.Remove(user);
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            User user1 = new User("a", "a", State.signin);
+            User user2 = new User("a", "a", State.signout);
+            User user3 = new User("a", "c", State.signin);
+            User user4 = new User("c", "c", State.signout);
+            User user5 = new User("e", "e", State.signin);
+
+            Console.Write($"Сравниваем пользователей 1 и 2 через Equals: {user1.Equals(user2)}\n");
+            Console.Write($"Сравниваем пользователей 1 и 2 через ==: {user1 == user2}\n");
+            Console.Write($"Сравниваем пользователей 3 и 4 через Equals: {user3.Equals(user4)}\n");
+            Console.Write($"Сравниваем пользователей 3 и 4 через ==: {user3 == user4}\n");
+            Console.Write($"Сравниваем пользователей 1 и 2 через Equals: {user5.Equals(user5)}\n");
+            Console.Write($"Сравниваем пользователей 5 и 1 через ==: {user5 == user1}\n");
+
+            WebNet github = new WebNet();
+            github.Add(user1);
+            github.Add(user2);
+            github.Add(user3);
+            github.Add(user4);
+            github.Add(user5);
+
+            int count = (from u in github.users
+                         where u.status == State.signin
+                         select u).Count();
+            Console.WriteLine($"Пользователей на сайте: {count}.\nСериализация...");
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(WebNet));
+            using (FileStream fs = new FileStream("data.json", FileMode.OpenOrCreate))
+            {
+                serializer.WriteObject(fs, github);
+            }
+
+            System.Diagnostics.Process.Start("data.json");
+            Console.ReadKey();
+        }
+    }
+}
